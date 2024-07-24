055de8a003d1678df989da66ef77088dd6792feb
RafaGms <faelgomes2003@gmail.com>
2024-07-23 21:16:38 -0300

    ✨feat: add edit component

---
+++ src/app/api/editApi.ts
@@ -0,0 +1,15 @@
+export const editTransactionApi = async (transactionId: number, updatedTransaction: { description: string, amount: number, type: string }) => {
+   const response = await fetch(`http://localhost:8000/transations/${transactionId}`, {
+      method: 'PUT',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedTransaction),
+   });
+
+   if (!response.ok) {
+      throw new Error('Erro ao editar transação');
+   }
+
+   return response.json();
+};
